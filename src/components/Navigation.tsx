f9bc540894fd078de6e823507a125312e1800933
sappnett <info.sappnett@gmail.com>
2025-11-01 16:39:24 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -48,7 +48,7 @@ export function Navigation() {
               </Link>
             ))}
             <Button variant="default" className="bg-gradient-neon glow-primary hover:glow-secondary" asChild>
-              <Link to="+18774968608"> (877) 419-5028</Link>
+              <Link to="/services"> (877) 419-5028</Link>
             </Button>
           </div>
 
